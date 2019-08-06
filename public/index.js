4f1ae469c01680cf8f2c45086a592aea1dcd0750
Glitch (jp-dub-night-owls) <none>
2019-08-06 20:08:52 +0000

    updated getLocation()

--- public/index.js
+++ public/index.js
@@ -110,35 +110,35 @@ class App extends Component {
     }
 
     getLocation(next) {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                var obj = {};
-                obj.latitude = position.coords.latitude;
-                obj.longitude = position.coords.longitude;
-               
-                next(obj);
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+           
+          postResults({
+            latitude : position.coords.latitude,
+            longitude: position.coords.longitude
+          });
+           
         }, showError);
-        } else {
-        console.log("Geolocation is not supported by this browser.");
-        };
-
-        const showError = (error) => {
-            switch(error.code) {
-                case error.PERMISSION_DENIED:
-                    console.log("User denied the request for Geolocation.");
-                    break;
-                case error.POSITION_UNAVAILABLE:
-                    console.log("Location information is unavailable.");
-                    break;
-                case error.TIMEOUT:
-                    console.log("The request to get user location timed out.");
-                    break;
-                case error.UNKNOWN_ERROR:
-                    console.log("An unknown error occurred.");
-                    break;
-            };
-        };
-
+      } else {
+        alert("Geolocation is not supported by this browser.");
+     };
+  
+     const showError = (error) => {
+       switch(error.code) {
+         case error.PERMISSION_DENIED:
+           console.log("User denied the request for Geolocation.");
+           break;
+         case error.POSITION_UNAVAILABLE:
+           console.log("Location information is unavailable.");
+           break;
+         case error.TIMEOUT:
+           console.log("The request to get user location timed out.");
+           break;
+         case error.UNKNOWN_ERROR:
+           console.log("An unknown error occurred.");
+           break;
+       };
+     };
     }      
 
     render() {
