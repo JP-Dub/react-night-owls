c9d030d736b26e9c4cfeea723f983db013ebbeb7
Glitch (jp-dub-night-owls) <none>
2019-08-14 14:08:11 +0000

    ☂️🚛 Checkpoint ./app/controllers/server.js:553280/67041

--- app/controllers/server.js
+++ app/controllers/server.js
@@ -17,6 +17,142 @@ function ClickHandler () {
     }
   };
   
+  // return all rsvps for users
+	this.getClicks = (req, res) => {
+    let nightlife = [];
+        
+    // check if id exists in nightlife array
+    function findId(id) {
+      for(let i = 0; i < nightlife.length; i++) {
+        if(nightlife[i].id === id) return i;
+      }              
+      return false;
+    };    
+		
+    Users
+			.find({}).select({ 'twitter.nightlife': 1, _id: false, id: 1, count: 1 })
+			.exec((err, results) => {
+				if (err) throw err;
+        console.log(results)
+        // return restaurant id and total 'going' count for all users
+        results.forEach((array, idx) => {
+          let arr = array.twitter.nightlife;
+        console.log(arr)
+          if(arr.length) {
+            for(let i = 0; i < arr.length; i++) {           
+              let item = arr[i];
+              nightlife.push({
+                'id'    : item.id,
+                'count' : item.count
+              }); 
+            }
+          }
+        });
+
+		res.json(nightlife);
+		}); 
+  }; // getClicks
+
+  // for authenticated users to add or remove rsvp  
+	this.addClick = (req, res) => {
+  
+		Users
+			.findOne({'twitter.id': req.body.userId})
+      .select({'twitter.nightlife': 1})
+			.exec((err, result) => {
+					if (err) throw err;      
+          
+          if(result) {
+            let nightlife = result.twitter.nightlife,
+                barCount = {},
+                barExists = false;
+            for(var i = 0; i < nightlife.length; i++) {
+                     
+               if(nightlife[i].id === req.body.id ) {
+                 nightlife[i].count = nightlife[i].count === 1 ? 0 : 1;
+                 barCount.id        = nightlife[i].id;                
+                 barCount.count     = nightlife[i].count === 0 ? -1 : 1;
+                 barExists          = true;
+               }
+            };                      
+            
+            if(!barExists) {
+              let obj = { 
+                id    : req.body.id,
+                name  : req.body.name,
+                count : 1
+                };
+              
+              // return obj to db and send barCount to UI
+              result.twitter.nightlife.push(obj); 
+              barCount = { id    : req.body.id, 
+                           count : 1
+                         };
+            }
+            
+            result.save( err => {
+              if(err) throw err;
+            });            
+            
+            res.json(barCount);
+         };
+
+			});
+	};
+	
+	// queries the Yelp api and stores session data and location
+	this.getNightlife = (req, res) => {
+		 var Client = yelp.client(process.env.API_KEY);
+     var request = {
+        		term    : 'bars',
+    	    	location: req.query.location,
+            sort_by : 'rating',
+            limit   : 20,
+        	};
+     
+     //if user authenticates, save location to user
+     if(req.body.user) { 
+       Users.findOneAndUpdate({ 
+            'twitter.id' : req.body.user
+            }, {
+            'twitter.previousSession' : req.query.location
+            }, {
+            new   : true, 
+            upsert: true
+            })
+          	.exec((err, success) => {
+             	if(err) return console.error(err);
+        	  });    
+     };
+        
+      // Yelp Fusion api	
+     Client.search(request).then(response => {
+       var results = response.jsonBody.businesses,
+           json    = JSON.stringify(results, null, 4);
+          
+           return res.json(json);
+     }).catch(error => {
+       	return res.json(error);
+    }); 
+	};
+	
+	// returns the user location and cached search results after twitter log in
+	this.userLocation = (req, res) => {
+   
+		Users.find({_id: req.user._id})
+			.exec((err, user) => {
+				if(err) throw err;       
+       
+        return res.json(user[0]);     				
+			});
+	}; 
+
+};
+
+module.exports = ClickHandler;
+
+
+/*
   this.demo = (req, res) => {
   let something = [
     {
@@ -965,148 +1101,7 @@ function ClickHandler () {
             }  
           };    
   };
-  
-	this.getClicks = (req, res) => {
-    let nightlife = [];
-        
-    // check if id exists in nightlife array
-    function findId(id) {
-      for(let i = 0; i < nightlife.length; i++) {
-        if(nightlife[i].id === id) return i;
-      }              
-      return false;
-    };    
-		
-    Users
-			.find({}).select({ 'twitter.nightlife': 1, _id: false})
-			.exec((err, results) => {
-				if (err) throw err;
-
-        // return restaurant id and total 'going' count for all users
-        results.forEach((array, idx) => {
-          let arr = array.twitter.nightlife;
-          if(arr.length) {
-            for(let i = 0; i < arr.length; i++) {
-              let item = arr[i];
-              if(item.count) {
-                let index = findId(item.id);
-                if(!index) {
-                   nightlife[index].count += item.count;
-                } else {
-                   nightlife.push({
-                     'id'    : item.id,
-                     'count' : item.count
-                   }); 
-                }
-              }
-            }
-          }
-        });
-
-		res.json(nightlife);
-		}); 
-  }; // getClicks
-
-  // for authenticated users to add or remove rsvp  
-	this.addClick = (req, res) => {
-  
-		Users
-			.findOne({'twitter.id': req.body.userId})
-      .select({'twitter.nightlife': 1})
-			.exec((err, result) => {
-					if (err) throw err;      
-          
-          if(result) {
-            let nightlife = result.twitter.nightlife,
-                barCount = {},
-                barExists = false;
-            for(var i = 0; i < nightlife.length; i++) {
-                     
-               if(nightlife[i].id === req.body.id ) {
-                 nightlife[i].count = nightlife[i].count === 1 ? 0 : 1;
-                 barCount.id        = nightlife[i].id;                
-                 barCount.count     = nightlife[i].count === 0 ? -1 : 1;
-                 barExists          = true;
-               }
-            };                      
-            
-            if(!barExists) {
-              let obj = { 
-                id    : req.body.id,
-                name  : req.body.name,
-                count : 1
-                };
-              
-              // return obj to db and send barCount to UI
-              result.twitter.nightlife.push(obj); 
-              barCount = { id    : req.body.id, 
-                           count : 1
-                         };
-            }
-            
-            result.save( err => {
-              if(err) throw err;
-            });            
-            
-            res.json(barCount);
-         };
-
-			});
-	};
-	
-	// queries the Yelp api and stores session data and location
-	this.getNightlife = (req, res) => {
-		 var Client = yelp.client(process.env.API_KEY);
-     var request = {
-        		term    : 'bars',
-    	    	location: req.query.location,
-            sort_by : 'rating',
-            limit   : 20,
-        	};
-     
-     //if user authenticates, save location to user
-     if(req.body.user) { 
-       Users.findOneAndUpdate({ 
-            'twitter.id' : req.body.user
-            }, {
-            'twitter.previousSession' : req.query.location
-            }, {
-            new   : true, 
-            upsert: true
-            })
-          	.exec((err, success) => {
-             	if(err) return console.error(err);
-        	  });    
-     };
-        
-      // Yelp Fusion api	
-     Client.search(request).then(response => {
-       var results = response.jsonBody.businesses,
-           json    = JSON.stringify(results, null, 4);
-          
-           return res.json(json);
-     }).catch(error => {
-       	return res.json(error);
-    }); 
-	};
-	
-	// returns the user location and cached search results after twitter log in
-	this.userLocation = (req, res) => {
-   
-		Users.find({_id: req.user._id})
-			.exec((err, user) => {
-				if(err) throw err;       
-       
-        return res.json(user[0]);     				
-			});
-	}; 
-
-};
-
-module.exports = ClickHandler;
-
-
-
+*/
 
 
 // 'use strict';
